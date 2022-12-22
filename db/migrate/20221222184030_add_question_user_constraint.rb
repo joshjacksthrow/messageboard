7f0c7788e48367cc0997469471705ebd4ad31de3
Joshua Jackson <joshua@collectivebenefits.com>
2022-12-22 17:40:09 +0100

    Create rubyonrails.yml
    Create rubyonrails.yml

--- db/migrate/20221222184030_add_question_user_constraint.rb
+++
@@ -1,6 +0,0 @@
-class AddQuestionUserConstraint < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :questions, :user
-    add_foreign_key :questions, :users
-  end
-end
---
+++ db/migrate/20221222211212_create_answers.rb
@@ -0,0 +1,11 @@
+class CreateAnswers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :answers do |t|
+      t.text :body
+
+      
+
+      t.timestamps
+    end
+  end
+end
